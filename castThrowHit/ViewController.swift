d3acd088ce75e66f934fce3cdce5e5c316ae535a
Trevor Hall <craftelectric@Trevors-MacBook-Pro.local>
2015-06-10 18:13:52 -0700

    Initial Commit

---
+++ castThrowHit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  castThrowHit
+//
+//  Created by Trevor Hall on 6/10/15.
+//  Copyright (c) 2015 Trevor Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
